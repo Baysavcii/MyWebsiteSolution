b9fba76a178309cf78312eacba3aa7d939e980e8
Baysavcii <lionfever06@gmail.com>
2025-02-18 16:01:02 +0300

    feat(AuthAPI): Implement JWT token authentication
    - Introduced IJwtTokenService and JwtTokenService for secure token generation.
    - Configured token expiration, issuer, and audience in appsettings.json.
    - Added role-based claims to the JWT payload for role-based authentication.

---
+++ MyWebsite.AuthAPI/Services/IJwtTokenService.cs
@@ -0,0 +1,9 @@
+﻿using MyWebsite.AuthAPI.Entities;
+
+namespace MyWebsite.AuthAPI.Services
+{
+    public interface IJwtTokenService
+    {
+        Task<string> GenerateTokenAsync(ApplicationUser user, IList<string> roles);
+    }
+}
