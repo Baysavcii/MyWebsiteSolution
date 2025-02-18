862718c685e5aecdc3c15c3e44cf67218bebe597
Baysavcii <lionfever06@gmail.com>
2025-02-18 12:48:56 +0300

    Add project files.

---
+++ MyWebsite.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyWebsite.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
