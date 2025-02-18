10121f6068f71c7da4596988a533b646650ada31
Baysavcii <lionfever06@gmail.com>
2025-02-18 14:56:47 +0300

    feat(AuthAPI): Setup database & Identity configuration
    - Added ApplicationDbContext for ASP.NET Core Identity
    - Implemented SeedData for user  and admin
    - Created ApplicationUser model
    - Configured Identity in Program.cs

--- MyWebsite.AuthAPI/Program.cs
+++ MyWebsite.AuthAPI/Program.cs
@@ -1,20 +1,46 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using MyWebsite.AuthAPI.Data;
+using MyWebsite.AuthAPI.Entities;
+using RestaurantApp.Common.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    try
+    {
+        var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+        await SeedData.Initialize(services, userManager);
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occured while seeding data to database");
+    }
+}
+
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+} 
 
 app.UseHttpsRedirection();
 
